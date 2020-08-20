6def7cc79858137acd77ffce827b1bbeb7eec19a
Sajeet Pokharel <sajeetpokh@gmail.com>
2020-08-20 20:47:11 +0545

    removed vs code and old urls file

--- src/articles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import ArticleViewSet
-
-router = DefaultRouter()
-router.register(r'article', ArticleViewSet)
-urlpatterns = router.urls
