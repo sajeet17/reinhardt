cf945df19d328dd4b8f25e9e4cb977a4f4a3a07b
Sajeet Pokharel <sajeetpokh@gmail.com>
2020-08-20 20:46:50 +0545

    moved vscode dir and old urls file

--- src/articles/api/urls.py
+++ src/articles/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from articles.api.views import ArticleViewSet
 from rest_framework.routers import DefaultRouter
+
+from articles.api.views import ArticleViewSet
+
 
 router = DefaultRouter()
 router.register(r'', ArticleViewSet, basename='articles')
